19a7d8a19d97559faacc0d0cc865c46d328c7903
DW <dfs1972@outlook.com>
2024-09-07 19:16:29 +0100

    First commit

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+//import { useCart } from "../../hooks/useCart";
+import classes from "./header.module.css";
+//import { useAuth } from "../../hooks/useAuth";
+
+export default function Header() {
+  const user = {
+    name: "DW",
+  };
+
+  const logout = () => {};
+
+  const cart = {
+    totalCount: 10,
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+          DGrub
+        </Link>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/dashboard">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
